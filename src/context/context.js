687d6d485b1de84d7d254d30aa5cdb990f327caf
lukemcgrath98 <lukemcgrath98@gmail.com>
2021-01-30 03:01:15 +0000

    Error testing for remaining requests to github API

--- src/context/context.js
+++ src/context/context.js
@@ -13,11 +13,37 @@ const GithubProvider = ({ children }) => {
     const [repos,setRepos] = useState(mockRepos);
     const [followers,setFollowers] = useState(mockFollowers);
 
-// request loading
+    // request loading
     const [requests, setRequests] = useState(0);
+    const [loading, setIsLoading] = useState(false);
+
+    //error
+    const [error,setError] = useState({show:false,msg:""});
+
+    // check rate
+const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+    .then(({data}) => {
+        let {rate:{remaining}} = data;
+        remaining = 0;
+        setRequests(remaining);
+        if(remaining == 0){
+            toggleError(true,'sorry you have exceeded your hourly rate limit!');
+        }
+    })
+    .catch((err) => console.log(err));
+};
+
+function toggleError(show,msg){
+    setError({show ,msg});
+}
+// error
+
+
+    useEffect(checkRequests, []);
 
     return (
-        <GithubContext.Provider value ={{githubUser,repos,followers}}>{children}</
+        <GithubContext.Provider value ={{githubUser,repos,followers,requests,error}}>{children}</
         GithubContext.Provider>
     );
 }
